27149d6f0567da8773406433bb5ed7d0afd5edf8
Nadine Görzen <namied@pm.me>
2023-10-23 15:41:24 +0200

    add task 2 (concurrency, poison pill)

--- src/aqua/blatt1/broker/Broker.java
+++ src/aqua/blatt1/broker/Broker.java
@@ -4,85 +4,116 @@ import aqua.blatt1.common.msgtypes.DeregisterRequest;
 import aqua.blatt1.common.msgtypes.HandoffRequest;
 import aqua.blatt1.common.msgtypes.RegisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterResponse;
+import aqua.blatt2.broker.PoisonPill;
 import messaging.Endpoint;
 import messaging.Message;
 
 import java.io.Serializable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Broker {
+    private static final int NUMTHREADS = 10;
     private final Endpoint endpoint = new Endpoint(4711);
-    private final ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
+    public ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
+    private final ExecutorService executor = newFixedThreadPool(NUMTHREADS);
     private int nextId = 0;
-    private boolean done = false;
+    private boolean stopRequested = false;
 
     public void broker() {
-        while (!done) {
+        Thread backgroundThread = new Thread(() -> {
+            showMessageDialog(null, "Press OK button to stop server");
+            stopRequested = true;
+        });
+        backgroundThread.start();
+
+        while (!stopRequested) {
             Message message = this.endpoint.blockingReceive();
             Serializable request = message.getPayload();
             InetSocketAddress sender = message.getSender();
-
-            if (request instanceof RegisterRequest) {
-                register(sender);
-            } else if (request instanceof DeregisterRequest) {
-                deregister(sender);
-            } else if (request instanceof HandoffRequest) {
-                handoffFish(sender, (HandoffRequest) request);
-            } else {
-                System.out.println("Unknown request: " + request);
-            }
+            executor.execute(new BrokerTask(request, sender));
         }
-    }
-
-    private void register(InetSocketAddress sender) {
-        if (clients.indexOf(sender) != -1) {
-            System.out.println("Client already registered: " + sender);
-            return;
-        }
-        clients.add("tank" + nextId, sender);
-        endpoint.send(sender, new RegisterResponse("tank" + nextId++));
-    }
-
-    private void deregister(InetSocketAddress sender) {
-        if (clients.indexOf(sender) == -1) {
-            System.out.println("Client not registered: " + sender);
-            return;
-        }
-        if (clients.size() == 1) {
-            done = true;
-        }
-        clients.remove(clients.indexOf(sender));
-    }
-
-    private void handoffFish(InetSocketAddress sender, HandoffRequest request) {
-        int vector = request.getFish().getDirection().getVector();
-        int index = clients.indexOf(sender);
-
-        switch (vector) {
-            case -1:
-                sendLeft(index, request);
-                break;
-            case 1:
-                sendRight(index, request);
-                break;
-            default:
-                System.out.println("Unknown vector: " + vector);
-                break;
-        }
-    }
-
-    private void sendLeft(int index, HandoffRequest request) {
-        InetSocketAddress leftNeighbor = clients.getLeftNeighborOf(index);
-        endpoint.send(leftNeighbor, request);
-    }
-
-    private void sendRight(int index, HandoffRequest request) {
-        InetSocketAddress rightNeighbor = clients.getRightNeighborOf(index);
-        endpoint.send(rightNeighbor, request);
+        executor.shutdown();
+        System.exit(0);
     }
 
     public static void main(String[] args) {
         new Broker().broker();
     }
 
+    private class BrokerTask implements Runnable {
+        private final ReadWriteLock lock = new ReentrantReadWriteLock();
+        private final Serializable request;
+        private final InetSocketAddress sender;
+
+        public BrokerTask(Serializable request, InetSocketAddress sender) {
+            this.request = request;
+            this.sender = sender;
+        }
+
+        @Override
+        public void run() {
+            if (request instanceof RegisterRequest) {
+                register(sender);
+            } else if (request instanceof DeregisterRequest) {
+                deregister(sender);
+            } else if (request instanceof HandoffRequest) {
+                handoffFish(sender, (HandoffRequest) request);
+            } else if (request instanceof PoisonPill) {
+                stopRequested = true;
+            } else {
+                System.out.println("Unknown request: " + request);
+            }
+        }
+
+        private void register(InetSocketAddress sender) {
+            lock.writeLock().lock();
+            if (clients.indexOf(sender) != -1) {
+                System.out.println("Client already registered: " + sender);
+                return;
+            }
+            clients.add("tank" + nextId, sender);
+            endpoint.send(sender, new RegisterResponse("tank" + nextId++));
+            lock.writeLock().unlock();
+        }
+
+        private void deregister(InetSocketAddress sender) {
+            lock.writeLock().lock();
+            if (clients.indexOf(sender) == -1) {
+                System.out.println("Client not registered: " + sender);
+                return;
+            }
+            if (clients.size() == 1) {
+                stopRequested = true;
+            }
+            clients.remove(clients.indexOf(sender));
+            lock.writeLock().unlock();
+        }
+
+        private void handoffFish(InetSocketAddress sender, HandoffRequest request) {
+            lock.readLock().lock();
+            int vector = request.getFish().getDirection().getVector();
+            int index = clients.indexOf(sender);
+            InetSocketAddress neighbor;
+
+            switch (vector) {
+                case -1:
+                    neighbor = clients.getLeftNeighborOf(index);
+                    break;
+                case 1:
+                    neighbor = clients.getRightNeighborOf(index);
+                    break;
+                default:
+                    System.out.println("Unknown vector: " + vector);
+                    return;
+            }
+            endpoint.send(neighbor, request);
+            lock.readLock().unlock();
+        }
+    }
 }
